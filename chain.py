5f088b51217ac27b503bb4602aa46a347370fb6a
cookooc <haw526@163.com>
2018-11-19 09:30:11 +0800

    链表结构分析

--- chain.py
+++ chain.py
@@ -3,3 +3,47 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+'''
+    链表相关
+'''
+
+
+class SimpleChain(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.child = None
+
+    def link(self, chain):
+        """
+            add chain
+        :param chain:
+        :return:
+        """
+        self.child = chain
+        return chain
+
+    # def __str__(self):
+    #     s_chain = self.value
+    #     chain = self.child
+    #     while chain:
+    #         s_chain = s_chain + '-->' + chain.value
+    #         chain = chain.child
+    #     return s_chain
+
+
+def reverse(node):
+    """
+
+    :return:
+    """
+    while node:
+        l
+
+
+sc = SimpleChain('10')
+sc.link(SimpleChain('127')).link(SimpleChain('0')).link(SimpleChain('11'))
+print sc
+print sc.reverse()
--- insertion.py
+++ insertion.py
@@ -3,3 +3,35 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+'''
+    插入排序
+    插入排序之死记硬背
+    1. 需要额外的存储 用来存储排序后的结果
+    2. 每次从原来列表中取出数据与有序的结果作比较
+'''
+
+
+def insertion(s):
+    """
+
+    :param s:
+    :return:
+    """
+    ds = []
+    for i in xrange(len(s)):
+        skip = False
+        for j in xrange(len(ds)):
+            if s[i] < ds[j]:
+                ds.insert(j, s[i])
+                skip = True
+                break
+        if not skip:
+            ds.append(s[i])
+    return ds
+
+
+o = [3, 5, 4, 5, 44, 38, 27, 49, 26, 46, 19, 47, 48]
+# print insertion(o)
+
